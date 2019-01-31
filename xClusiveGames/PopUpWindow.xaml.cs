6fe13cf5dd17fb4baafe4deb5aad48b6634c962b
Victor Heine Holten <vict1856@edu.eal.dk>
2019-01-31 13:51:24 +0100

    All done

--- xClusiveGames/PopUpWindow.xaml.cs
+++ xClusiveGames/PopUpWindow.xaml.cs
@@ -19,10 +19,42 @@ namespace xClusiveGames
     /// </summary>
     public partial class PopUpWindow : Window
     {
+        bool Switch = false;
         public PopUpWindow()
         {
             InitializeComponent();
+            Trailer.Source = new Uri(@"C:\Users\Victor\Downloads\trailer.wmv");
+            PlayButton.Opacity = 0;
             Trailer.Play();
+        }
+
+        private void Trailer_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Switch == false)
+            {
+                Trailer.Pause();
+                Switch = true;
+                
+            }
+            else
+            {
+                Trailer.Play();
+                Switch = false;
+            }
+            
+        }
+
+        private void Trailer_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PlayButton.Opacity = 0.5;
+        }
+
+        private void Trailer_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (Switch == false)
+            {
+                PlayButton.Opacity = 0;
+            }
         }
     }
 }
